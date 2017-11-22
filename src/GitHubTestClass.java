ad8392b9eb63c7e7a8a81f1c4bb3c6d488a172cd
Alan Kostrick <akostrick@hotmail.com>
2017-11-22 16:17:44 -0500

    add some more code

--- src/GitHubTestClass.java
+++ src/GitHubTestClass.java
@@ -4,6 +4,8 @@ public class GitHubTestClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello GitHub!");
+		System.out.println("I am doing more code!");
+		System.out.println("Code code code!!!!");
 
 	}
 
